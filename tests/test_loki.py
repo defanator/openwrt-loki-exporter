20b6089dfc557d0a5321ff55103c3617e3e2f777
Andrei Belov <defanator@gmail.com>
2024-10-20 19:06:47 +0400

    tests: downgrade loki to 2.9.2, add some debug

--- tests/test_loki.py
+++ tests/test_loki.py
@@ -5,11 +5,15 @@ Base tests for loki_exporter
 from datetime import datetime, timedelta, timezone
 from client import BASE_URL, query_labels, query_range
 
+# from client import flush
+
 
 def test_labels():
     """
     Check available labels and match with expectation
     """
+    # assert flush(BASE_URL) is True, "flush succeeded"
+
     response = query_labels(BASE_URL)
 
     assert "status" in response.keys()
@@ -29,6 +33,9 @@ def test_line_count():
     """
     Verify that loki returns expected number of processed lines
     """
+
+    # assert flush(BASE_URL) is True, "flush succeeded"
+
     query = '{job="openwrt_loki_exporter"}'
     current_time = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
     start_time = (datetime.now(timezone.utc) - timedelta(hours=2)).strftime(
@@ -48,13 +55,24 @@ def test_line_count():
     assert "result" in data.keys()
     result = data.get("result")
     assert isinstance(result, list)
-    assert len(result) == 1
+    # assert len(result) == 1
+
+    l = []
 
     total_entries = 0
     for h in result:
         for k, v in h.items():
             if k == "values":
                 total_entries += len(v)
+                for entry in v:
+                    ts = int(entry[0]) / 10**9
+                    msg = entry[1]
+                    l.append({"ts": ts, "msg": msg})
+
+    l = sorted(l, key=lambda d: d["ts"])
+    with open("tests/resurrected.log", "w", encoding="utf-8") as file:
+        for entry in l:
+            file.write(f"{entry['ts']:.3f}: {entry['msg']}\n")
 
     assert total_entries == 491
 
